1501d4a1df11ee7f8ed3857b27d4979130cff842
anhgalxag2004 <anhgalxag2004@gmail.com>
2025-11-16 19:04:39 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://ia04-fe-1.onrender.com'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
